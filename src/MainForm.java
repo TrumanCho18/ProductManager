612cc642f9b9139043b2a0ed691398d142ba29dd
Truman Cho <tlc101809@gmail.com>
2026-05-03 22:39:34 -0400

    GUI edits

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,21 +1,38 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
 
     private JPanel MainWindow;
     private JLabel HeaderText;
-    private JButton button1;
-    private JPanel TopPanel;
-    private JPanel MidPanel;
-
+    private JPanel Home;
+    private JPanel AddProductScreen;
+    private JButton addProductButton;
+    private JLabel AddProduct;
+    private JTextField Name;
+    private JTextField Cost;
+    private JTextField Sold;
+    private JButton addButton;
 
     public MainForm() {
 
         setContentPane(MainWindow);
         HeaderText.setBorder(new EmptyBorder(30, 30, 30, 30));
-        button1.setSize(50, 50);
 
+        CardLayout c = new CardLayout();
+        MainWindow.setLayout(c);
+
+        addProductButton.setBorderPainted(false);
+        addProductButton.setFocusPainted(false);
+        addProductButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                c.next(MainWindow);
+            }
+        });
 
         setTitle("ProductManager(v.beta)");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -27,6 +44,7 @@ public class MainForm extends JFrame {
 
     public static void main(String[] args) {
         new MainForm();
-
+        CardLayout c = new CardLayout();
     }
+
 }
