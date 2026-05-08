129a4d916f33383f33de2c5d1b34b2f743a9dfb0
Truman Cho <tlc101809@gmail.com>
2026-05-07 23:36:29 -0400

    GUI edits

--- src/MainForm.java
+++ src/MainForm.java
@@ -3,6 +3,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class MainForm extends JFrame {
 
@@ -18,6 +20,7 @@ public class MainForm extends JFrame {
     private JButton addButton;
     private JLabel Status;
     private JSpinner qty;
+    private JTextField Date;
 
     public MainForm() {
 
@@ -40,7 +43,21 @@ public class MainForm extends JFrame {
         Name.setBorder(null);
         Sold.setBorder(null);
         Cost.setBorder(null);
+        Date.setBorder(null);
+
         qty.setBorder(null);
+        JComponent editor = qty.getEditor();
+        JFormattedTextField textField = ((JSpinner.DefaultEditor) editor).getTextField();
+        textField.setBackground(Color.GRAY);
+        textField.setForeground(Color.WHITE);
+        qty.addChangeListener(e -> {
+            int val = (int) qty.getValue();
+            if (val < 1) {
+                textField.setBackground(Color.RED);
+            } else {
+                textField.setBackground(Color.GRAY);
+            }
+        });
 
         addButton.setBorderPainted(false);
         addButton.setFocusPainted(false);
@@ -52,6 +69,73 @@ public class MainForm extends JFrame {
             }
         });
 
+        Name.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (Name.getText().equals("Name")) {
+                    Name.setText("");
+                    Name.setForeground(Color.WHITE);
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (Name.getText().isEmpty()) {
+                    Name.setForeground(Color.LIGHT_GRAY);
+                    Name.setText("Name");
+                }
+            }
+        });
+
+        Sold.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (Sold.getText().equals("Sell Price")) {
+                    Sold.setText("");
+                    Sold.setForeground(Color.WHITE);
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (Sold.getText().isEmpty()) {
+                    Sold.setForeground(Color.LIGHT_GRAY);
+                    Sold.setText("Sell Price");
+                }
+            }
+        });
+
+        Cost.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (Cost.getText().equals("Cost")) {
+                    Cost.setText("");
+                    Cost.setForeground(Color.WHITE);
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (Cost.getText().isEmpty()) {
+                    Cost.setForeground(Color.LIGHT_GRAY);
+                    Cost.setText("Cost");
+                }
+            }
+        });
+
+        Date.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (Date.getText().equals("Date Logged")) {
+                    Date.setText("");
+                    Date.setForeground(Color.WHITE);
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (Date.getText().isEmpty()) {
+                    Date.setForeground(Color.LIGHT_GRAY);
+                    Date.setText("Date Logged");
+                }
+            }
+        });
 
         setTitle("ProductManager(v.beta)");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
